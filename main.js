e112a3767b2b3d9c2a6d1e896d20d40a16c991b1
Colton <allen.colton@hotmail.com>
2023-07-07 19:25:45 -0700

    added player and controls

--- main.js
+++ main.js
@@ -1,24 +1,109 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const canvas = document.querySelector('canvas#world');
+const ctx = canvas.getContext('2d');
+import playerImg from './images/characters/people/hero.png';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+class Game {
+  constructor(){
+    this.frame = 0;
+  }
+}
+const game = new Game();
 
-setupCounter(document.querySelector('#counter'))
+class Sprite {
+  constructor(x, y, src, cols, rows) {
+    this.loaded = false;
+    this.position = { x, y };
+    this.speed = 1;
+    this.cols = cols;
+    this.rows = rows;
+    this.frame = {
+      x: 0,
+      y: 0,
+      w: null,
+      h: null
+    };
+    this.src = src;
+    this.image = new Image();
+    this.image.src = this.src;
+    this.image.onload = () => {
+      this.frame.w = this.image.width / this.cols;
+      this.frame.h = this.image.height / this.rows;
+      this.init(ctx);
+    };
+    this.controls = {
+      j: false,
+      l: false,
+      k: false,
+      h: false,
+    };
+    this.lastDirection = 'j';
+    this.directionArr = ['j','l','k','h'];
+  }
+  init(ctx) {
+    this.loaded = true;
+  }
+  draw(ctx) {
+    ctx.drawImage(
+      player.image, 
+      this.frame.x * this.frame.w,
+      this.frame.y * this.frame.h,
+      this.frame.w,
+      this.frame.h,
+      this.position.x, 
+      this.position.y,
+      this.frame.w,
+      this.frame.h,
+    );
+  }
+  update(ctx) {
+    if (this.controls.j && !this.controls.k) {
+      this.position.y += this.speed;
+      this.lastDirection = 'j';
+    }
+    if (!this.controls.j && this.controls.k) {
+      this.position.y -= this.speed;
+      this.lastDirection = 'k';
+    }
+    if (this.controls.h && !this.controls.l) {
+      this.position.x -= this.speed;
+      this.lastDirection = 'h';
+    }
+    if (!this.controls.h && this.controls.l) {
+      this.position.x += this.speed;
+      this.lastDirection = 'l';
+    }
+    this.frame.y = this.directionArr.indexOf(this.lastDirection);
+    this.draw(ctx);
+  }
+}
+
+const player = new Sprite(canvas.width / 2, canvas.height / 2, playerImg, 4, 4);
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (game.frame % 12 === 0) {
+    player.frame.x >= player.cols - 1 ? player.frame.x = 0 : player.frame.x++;
+  }
+  if (player.loaded) {
+    player.update(ctx);
+  }
+
+  game.frame++;
+
+  requestAnimationFrame(animate);
+}
+animate();
+
+addEventListener('keydown', handleKeydown);
+addEventListener('keyup', handleKeyup);
+
+function handleKeydown(e) {
+  if (!['h','j','k','l'].includes(e.key)) return;
+  player.controls[e.key] = true;
+}
+
+function handleKeyup(e) {
+  if (!['h','j','k','l'].includes(e.key)) return;
+  player.controls[e.key] = false;
+}
